2d9ff7c640d7349dc8198379e72cf755b39d81ef
Gusram <idabgsram@gmail.com>
2024-04-19 10:39:43 +0800

    up client-server stream grpc

---
+++ pertemuan12/client-server-stream-grpc/go.mod
@@ -0,0 +1,16 @@
+module sekolahbeta/pertemuan12/grpc
+
+go 1.21.8
+
+require (
+	github.com/sirupsen/logrus v1.9.3
+	google.golang.org/grpc v1.63.0
+	google.golang.org/protobuf v1.33.0
+)
+
+require (
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+)
---
+++ pertemuan12/client-server-stream-grpc/server/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sekolahbeta/pertemuan12/grpc/proto"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+type GreetService struct {
+	proto.GreetServiceServer
+}
+
+func (s *GreetService) Greet(ctx context.Context, req *proto.GreetRequest) (*proto.GreetResponses, error) {
+
+	userName := req.Username
+	phone := req.Phone
+
+	response := fmt.Sprintf("Hello %s, no hp mu %d!", userName, phone)
+
+	return &proto.GreetResponses{
+		Greetresponse: response,
+	}, nil
+
+}
+
+func (s *GreetService) StoreStream(srv proto.GreetService_StoreStreamServer) error {
+
+	for {
+		stream, err := srv.Recv()
+		if err == io.EOF {
+			return nil
+		} else if err == nil {
+			fmt.Printf("Menerima data dari client : %s\n",
+				stream.Username)
+		} else {
+			return err
+		}
+	}
+}
+
+func (s *GreetService) GreetStream(req *proto.GreetStreamRequest,
+	srv proto.GreetService_GreetStreamServer) error {
+
+	log.Println("Memulai mengirim data")
+
+	res := strings.Split(req.Username, ",")
+
+	for i, usrname := range res {
+		resp := proto.GreetStreamResponses{
+			Part:    int64(i + 1),
+			Message: fmt.Sprintf("Hallo %s", usrname),
+		}
+		if err := srv.Send(&resp); err != nil {
+			log.Printf("Terjadi error mengirim data, err:%s\n",
+				err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("Memulai gRPC Server ...")
+	grpcListener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		logrus.Fatalf("Gagal menginisialisasi server, error : %s", err.Error())
+	}
+
+	grpcServer := grpc.NewServer()
+	proto.RegisterGreetServiceServer(grpcServer, &GreetService{})
+	grpcServer.Serve(grpcListener)
+}
